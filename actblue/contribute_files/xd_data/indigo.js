b8b78fc9ba7ecec3df4cee4f294631cfa3892bf1
Aaron Lipman <alipman88@gmail.com>
2020-01-09 13:31:01 -0500

    remove unused sites

--- actblue/contribute_files/xd_data/indigo.js
+++
@@ -1,1 +0,0 @@
-var xhrCaller=function(){return window.XMLHttpRequest?new XMLHttpRequest:window.ActiveXObject?activeXCaller():null},activeXCaller=function(){var a=["Msxml2.XMLHTTP","Microsoft.XMLHTTP","MSXML2.XMLHTTP.3.0"];for(var b=0;b<a.length;b++)try{return new ActiveXObject(activexmodes[b])}catch(c){}return null};
